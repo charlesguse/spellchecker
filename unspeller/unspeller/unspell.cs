0fa642b300aafe8d0e43330773f76c0982648f0e
Charles <Charles@Animus.hsd1.in.comcast.net.>
2011-12-29 17:57:58 -0500

    Added the unspeller project. Started doing work on it. Still have a fair amount to do.

---
+++ unspeller/unspeller/unspell.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace unspeller
+{
+    public struct CharacterToChange
+    {
+        public int LetterIndex;
+        public int NewCharacter;
+    }
+
+    public class unspell
+    {
+        public static char[] Vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+
+        static public string RepeatLetter(string word, int letterIndex, int timesToRepeat)
+        {
+            StringBuilder newWord = new StringBuilder(word);
+            char charToRepeat = newWord[letterIndex];
+            String repeatedChars = new String(charToRepeat,timesToRepeat);
+            newWord.Insert(letterIndex, repeatedChars);
+
+            return newWord.ToString();
+        }
+
+        public static string ChangeCase(string word, int letterIndex)
+        {
+            StringBuilder newWord = new StringBuilder(word);
+
+            if (char.IsLower(newWord[letterIndex]))
+                newWord[letterIndex] = char.ToUpper(newWord[letterIndex]);
+            else
+                newWord[letterIndex] = char.ToLower(newWord[letterIndex]);
+
+
+            return newWord.ToString();
+        }
+
+        public static string ChangeCharacter(string word, int letterIndex, char newCharacter)
+        {
+            StringBuilder newWord = new StringBuilder(word);
+            newWord[letterIndex] = newCharacter;
+            return newWord.ToString();
+        }
+
+        //public static CharacterToChange FindVowelToChange(string word)
+        //{
+        //    CharacterToChange vowel;
+        //    // If the loop
+        //    vowel.LetterIndex = 0;
+        //    vowel.NewCharacter = word[0];
+
+        //    for (int i = 0; i < word.Length; i++)
+        //    {
+
+        //    }
+        //}
+
+        // The vowel related stuff is close to identical in both applications.
+        // Normally I wouldn't copy and paste code from one spot to another.
+        // However, since these are seperate applications, I didn't want to
+        // have them linked together and I didn't want to create a library
+        // of one to two functions.
+        private static bool IsVowel(char character)
+        {
+            foreach (char vowel in Vowels)
+            {
+                if (char.ToLower(character) == vowel)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ unspeller/unspellerUnitTests/unspellerTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using unspeller;
+
+namespace unspellerUnitTests
+{
+    [TestFixture]
+    public class unspellerTests
+    {
+        [Test]
+        public void RepeatNoLetterTest()
+        {
+            Assert.AreEqual("sheep", unspell.RepeatLetter("sheep", 0, 0));
+        }
+
+        [Test]
+        public void RepeatLetterTest()
+        {
+            Assert.AreEqual("sheeeeep", unspell.RepeatLetter("sheep", 2, 3));
+        }
+
+        [Test]
+        public void RepeatMultipleLettersTest()
+        {
+            var actual = unspell.RepeatLetter("job", 0, 1);    // jjob
+            actual = unspell.RepeatLetter(actual, 2, 1);       // jjoob
+            actual = unspell.RepeatLetter(actual, 4, 2);       // jjoobbb
+            Assert.AreEqual("jjoobbb", actual);
+        }
+
+        [Test]
+        public void ChangeCaseFromLowerToUpperTest()
+        {
+            Assert.AreEqual("Inside", unspell.ChangeCase("inside", 0));
+        }
+
+        [Test]
+        public void ChangeCaseFromUpperToLowerTest()
+        {
+            Assert.AreEqual("insIDE", unspell.ChangeCase("inSIDE", 2));
+        }
+
+        [Test]
+        public void ChangeCaseOfDifferentCultureTest() 
+        {
+            // They are in my dictionary, so I figured
+            // I would see how changing the case worked with
+            // non-english characters.
+            Assert.AreEqual("å", unspell.ChangeCase("Å", 0));
+        }
+
+        [Test]
+        public void ChangeCharacterTest()
+        {
+            Assert.AreEqual("weke", unspell.ChangeCharacter("wake", 1, 'e'));
+        }
+    }
+}
